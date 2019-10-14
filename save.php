aa84cc3aa263747491b6d122cda2a996d2fa9237
chade7160 <chade7160@gmail.com>
2019-10-14 14:54:29 -0500

    First PHP practice

---
+++ save.php
@@ -0,0 +1,11 @@
+<?
+    echo "
+    이름: $name <br>
+    비밀번호: $pass <br>
+    결혼 여부: $merry <br>
+    차량소유: $car <br>
+    학벌: $study <br>
+    자기소개: $intro <br>
+    등급: $grade <br>
+    ";
+    ?>
